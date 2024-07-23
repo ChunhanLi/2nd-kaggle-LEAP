dfefbe648e32546a3420280f1f2d7cdd906c8200
Midone. <chunhanli1995@gmail.com>
2024-07-23 10:28:55 +0800

    fixbug

--- adam_part/src/cp_infer.sh
+++ adam_part/src/cp_infer.sh
@@ -1,3 +1,3 @@
-cp ./infer/subs/exp195_new.parquet ../../submission/subs/adam_195_78569.oof.npy
-cp ./infer/subs/exp197_new.parquet ../../submission/subs/adam_197_78551.oof.npy
-cp ./infer/subs/exp200_new.parquet ../../submission/subs/adam_200_78362.oof.npy
+cp ./infer/subs/exp195_new.parquet ../../submission/subs/adam_195_78569.oof.parquet
+cp ./infer/subs/exp197_new.parquet ../../submission/subs/adam_197_78551.oof.parquet
+cp ./infer/subs/exp200_new.parquet ../../submission/subs/adam_200_78362.oof.parquet
--- adam_part/src/cp_train.sh
+++ adam_part/src/cp_train.sh
@@ -1,3 +1,3 @@
-cp ./outputs/195/exp195_new.parquet ../../submission/subs/adam_195_78569.oof.npy
-cp ./outputs/197/exp197_new.parquet ../../submission/subs/adam_197_78551.oof.npy
-cp ./outputs/200/exp200_new.parquet ../../submission/subs/adam_200_78362.oof.npy
+cp ./outputs/195/exp195_new.parquet ../../submission/subs/adam_195_78569.oof.parquet
+cp ./outputs/197/exp197_new.parquet ../../submission/subs/adam_197_78551.oof.parquet
+cp ./outputs/200/exp200_new.parquet ../../submission/subs/adam_200_78362.oof.parquet
