8c3f8397281a997db10827a48bc9c1b83cf092cf
Duan MingJiang <duanmj@zju.edu.cn>
2024-07-23 10:53:22 +0800

    add cp command in max2020 script

--- fdzm_part/infer/infer_max2020.sh
+++ fdzm_part/infer/infer_max2020.sh
@@ -3,3 +3,9 @@ python 014_group_finetune.py
 python 015_group_finetune.py
 python 021_fork14_moredim_gf.py
 python 022_fork14_deeper_gf.py
+
+cp ../infer_outputs/010_group_finetune/exp010_group_finetune.parquet ../../submission/subs/max_010_gf_cv7888_lb7861.parquet
+cp ../infer_outputs/014_group_finetune/exp014_group_finetune.parquet ../../submission/subs/max_014_gf_cv7905_lb7873.parquet
+cp ../infer_outputs/015_group_finetune/exp015_group_finetune.parquet ../../submission/subs/max_015_gf_cv7905_lb7878.parquet
+cp ../infer_outputs/021_fork14_moredim_gf/exp021_fork14_moredim_gf.parquet ../../submission/subs/max_021_moredim_gf_cv7904_lbUnknown.parquet
+cp ../infer_outputs/022_fork14_deeper_gf/exp022_fork14_deeper_gf.parquet ../../submission/subs/max_022_deeper_gf_cv7908_lbUnknown.parquet
